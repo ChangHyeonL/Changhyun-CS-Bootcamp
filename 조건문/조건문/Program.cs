f7023d47500e2dfb54d17fde6eaa75090ae1eab6
ChangHyeonL <ckdgus48@gmail.com>
2025-08-29 12:04:49 +0900

    feat: 조건문 학습
    - if, else, else if, Switch 학습

---
+++ 조건문/조건문/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 조건문
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int min = 0;
+            int max = 5;
+            Console.WriteLine("숫자를 입력하세요.1");
+            string val = Console.ReadLine();
+            int c = int.Parse(val);
+            Console.WriteLine("c 값은" + c);
+            //비교연산자, 논리연산자, bool 값
+            switch(c)
+            {
+                case 0:
+                    PrintSheet();
+                    break;
+                case 1:
+                    ReadData();
+                    break;
+                case 2:
+                    break;
+                default:
+                    Console.WriteLine("해당사항 없습니다");
+                    break;
+            }
+        }
+
+        private static void ReadData()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintSheet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
