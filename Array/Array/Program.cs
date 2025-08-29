810c481660a0a852ce2a6172edf5a5b757cb05e1
ChangHyeonL <ckdgus48@gmail.com>
2025-08-29 11:24:34 +0900

    feat: 배열 기초 학습
    - 배열 기초 학습

---
+++ Array/Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[35];
+            students[0] = new Student("lee");
+            students[1] = new Student("kim");
+            students[33] = null;
+
+            Student[] students_1 = new Student[30];
+            Student[] students_2 = new Student[30];
+            Student[] students_3 = new Student[30];
+            //string[] name = new string[] { "홍길동", "김길동", "박길동" };
+            //int[,] kor_2 = new int[,] { { 10, 20, 30 }, { 30, 40, 50 }, { 40, 50, 60 } };
+            //int[,,] kor_3 = new int[,,] { { { 10, 20, 30 }, { 30, 40, 50 }, { 40, 50, 60 } }, { { 10, 20, 30 }, { 30, 40, 50 }, { 40, 50, 60 } }, { { 10, 20, 30 }, { 30, 40, 50 }, { 40, 50, 60 } } };
+        }
+
+        class Student
+        {
+            private int kor;
+            private string name;
+            public Student(string name)
+            {
+                this.name = name;
+            }
+        }
+    }
+}
